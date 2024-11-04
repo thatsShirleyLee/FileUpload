657569ed3cc8feed57d598185bffcf9f03353e32
thatsShirleyLee <dhudblab@163.com>
2024-11-04 17:43:16 +0800

    init

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'App',
+})
+  
+</script>
+
+<template>
+  <div>
+    <h1>大文件分片上传</h1>
+    <input type="file">
+  </div>
+</template>
+
+<style scoped lang="scss">
+  
+</style>
