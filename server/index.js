0b41973c2bc066a7a8c6342b5637f8b2b9e66db3
thatsShirleyLee <dhudblab@163.com>
2024-11-04 22:17:29 +0800

    update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/post', (req, res) => {
+    return res.send('Hello World!');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
